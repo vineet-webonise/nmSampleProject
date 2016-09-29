8bbcc3cb67bd3c5db6765c171a1c406ceab0be5b
admin <admin@Ranvijay-mac.local>
2016-09-29 16:07:50 +0530

    Project Files setup

---
+++ nmSampleProject/nmSampleProject/Common/CommonConstants.swift
@@ -0,0 +1,13 @@
+//
+//  CommonConstants.swift
+//  nmSampleProject
+//
+//  Created by admin on 29/09/16.
+//  Copyright © 2016 admin. All rights reserved.
+//
+
+import UIKit
+
+class CommonConstants: NSObject {
+
+}
